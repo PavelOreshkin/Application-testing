0c1e7837d616b0c6f101685e0e9a7cc4d60c8133
Pavel_Oreshkin <lacorza7@gmail.com>
2019-05-28 16:06:41 +0400

    mounting App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,32 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 // import { shallowToJson } from 'enzyme-to-json';
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import App from './App';
-// configure({ adapter: new Adapter() });
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
+import App from './App';
+configure({ adapter: new Adapter() });
 
+describe('<App>', () => {
+  const initialState = {
+    isFetching: false,
+    error: false,
+    users: []
+  };
 
-// describe('<App/>', () => {
-//   // it('should render correctly', () => {
-//   //   const output = shallow(<App />);
-//   //   expect(shallowToJson(output)).toMatchSnapshot();
-//   // });
-// });
+  const mockStore = configureStore();
+  let store, container;
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+    container = shallow(<App store={store} />);
+  })
+
+  it('render component', () => {
+    expect(container.length).toEqual(1);
+  });
+
+  it('check prop matches with initialState', () => {
+    expect(container.prop('output')).toEqual(initialState.output);
+  });
+})
